d770fda80962edf723a9581fcd647d0f552fa164
yadukrishnakp <yadukrishnakp007@gmail.com>
2021-04-16 16:06:36 +0530

    made a database with a table named by User

--- venv/project/app.py
+++ venv/project/app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template, url_for, redirect
-
-from models import LoginForm
+from models import LoginForm, RegistrationForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(40), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,10 +26,13 @@ def home():
     return render_template('home.html', form=form)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    return render_template('login.html', form=form)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        
+        return redirect('/register')
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
--- venv/project/models.py
+++ venv/project/models.py
@@ -1,18 +1,34 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms.validators import DataRequired, Length, ValidationError, InputRequired, Email, EqualTo
 import re
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=15)])
     submit = SubmitField('Login')
 
     def password_validation(self, password):
         if re.search('[0-9]', password) is None:
             raise ValidationError('Make sure your password has a number in it')
-        elif re.search('[A-Z]', password) is None:
+        if re.search('[A-Z]', password) is None:
             raise ValidationError('Make sure your password has a capital letter in it')
-        else:
-            raise ValidationError("Your password seems fine")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=15)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password'), Length(min=6, max=15)])
+    submit = SubmitField('Sign Up')
+
+    # def validate_username(self, username):
+    #     user = User.query.filter_by(username=username.data).first()
+    #     if user:
+    #         raise ValidationError('That username is taken.Please choose different one')
+    #
+    # def validate_email(self, email):
+    #     email = User.query.filter_by(email=email.data).first()
+    #     if email:
+    #         raise ValidationError('That email is taken.Please choose different one')
